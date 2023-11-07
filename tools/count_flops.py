da6711b592a7befbf74f7b6c34c2d3570647de7b
npurson <natterpurson@qq.com>
2023-11-07 17:35:41 +0800

    fix(flop_count): switch model to eval

--- tools/count_flops.py
+++ tools/count_flops.py
@@ -15,8 +15,10 @@ def main(cfg: DictConfig):
 
     dls, meta_info = build_data_loaders(cfg.data)
     model = LitModule(**cfg, meta_info=meta_info)
+    model.eval()
     flops = FlopCountAnalysis(model, next(iter(dls[1]))[0])
     print(flop_count_table(flops))
+    # print(flops.total())
 
 
 if __name__ == '__main__':
